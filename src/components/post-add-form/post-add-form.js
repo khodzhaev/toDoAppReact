9294d94d93aa106d79660a93f7c99cbc660505a8
khozhiakbar <khozhiakbar@gmail.com>
2020-05-11 22:47:52 +0600

    add files

---
+++ src/components/post-add-form/post-add-form.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+import './post-add-form.css';
+export default class PostAddForm extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        };
+        this.onValueChange = this.onValueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    onValueChange(e) {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        this.props.onAdd(this.state.text);
+        this.setState({
+            text: ''
+        })
+    }
+
+    render() {
+        return (
+        <form
+            className="bottom-panel d-flex"
+            onSubmit={this.onSubmit}>
+            <input value={this.state.text} onChange={this.onValueChange} type="text" placeholder="О чем вы думаете сейчас" className="form-control new-post-label"/>
+            <button className="btn btn-outline-secondary" type="submit" >
+                Добавить
+            </button>
+        </form>
+    )
+    }
+
+}
---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./post-status-filter.css";
+
+export default class PostStatusFilter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.buttons = [
+            {name: 'all', label: "Все"},
+            {name: 'like', label: "Понравилось"}
+        ]
+    }
+
+    render() {
+        const buttons = this.buttons.map(({name, label}) => {
+            const {filter, onFilterSelect} = this.props;
+            const active = filter === name;
+            const clazz = active ? 'btn-info' : 'btn-outline-secondary';
+            return (
+                <button
+                    key={name}
+                    className={`btn ${clazz}`}
+                    type="button"
+                    onClick={() => onFilterSelect(name)}>
+                    {label}
+                </button>
+            )
+        });
+        return (
+            <div className="btn-group">
+                {buttons}
+            </div>
+        )
+    }
+}
